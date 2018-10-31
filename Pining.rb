72ecfc44631fc5ddb1a799fcd1ac168125b6b8f6
ghr85 <ghrenfrew@gmail.com>
2018-10-31 06:02:31 +0000

    First commit

---
+++ Pining.rb
@@ -0,0 +1,33 @@
+puts 1 + 2
+sleep 1
+puts 2.0 + 1
+sleep 1
+puts 2.0 + 1.0
+sleep 1
+puts 2 + 1
+sleep 1
+puts "2" + "1"
+sleep 1
+puts -2 + -1
+sleep 1
+yearhour = 365 * 24
+sleep 2
+puts "in a year there are: #{yearhour} hours"
+sleep 2
+string1 = "Henry the 8th I am I Am"
+puts string1
+sleep 2
+puts string1
+puts "Mind that time we got wrecked and sang #{string1}"
+sleep 1
+puts "No."
+sleep 2
+puts "No I do not."
+string1 = "When 2 become 1"
+sleep 1
+puts "In fact all i remember is you stealing yer da's stockings and singing the Spice Girls 1999 hit #{string1}"
+string1 = "5"
+puts string1.to_i
+puts 5 + string1.to_i
+pishy = [12,3,4,5,"ab",6,]
+puts pishy[4]
